d2c0442353febea81f8e852c72db1fd5728453a9
marty-gleason <officergleason@gmail.com>
2018-10-09 06:48:18 -0500

    10-9-2018 second build

--- wordcloud.R
+++ wordcloud.R
@@ -14,15 +14,14 @@ source("../scripts/color_scheme.R")
 gs_auth()
 responses <- gs_url("https://docs.google.com/spreadsheets/d/1bN3ZZ1hVDD_VDCEtYmMYnIocaWkHn1gOsKs5r5Dt44A/edit#gid=1528966377")
 
-<<<<<<< HEAD
 responses <- gs_url(url)
 
 sheet <- gs_read(responses)
 email <- sheet$`Email Address`
-=======
+
 gsheet <- gs_read(responses)
 email <- gsheet$`Email Address`
->>>>>>> master
+
 length(unique(email))
 
 words_untidy <- gsheet[7]
